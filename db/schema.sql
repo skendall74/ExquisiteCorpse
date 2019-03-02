9842618f7eb83e24f12aaeeed899e25c01679658
Mat King <mat@matking.info>
2019-03-02 18:29:10 -0500

    schema started

---
+++ db/schema.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE ecorpse_p2;
+USE ecorpse_p2;
+
+CREATE TABLE `elements` (
+  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+  `body` VARCHAR( 255) NOT NULL,
+  'commit' BOOLEAN,
+  `user_id` VARCHAR( 255 ) NOT NULL,
+  `story_id` DATETIME NOT NULL,
+--   Created_at | Sequlized Will Populate these.
+--   Updated_at | Sequalized Will Populate these.
+  PRIMARY KEY ( `id` ) 
+);
+
+CREATE TABLE `users` (
+  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+  `first_name` VARCHAR( 255) NOT NULL,
+  `last_name` VARCHAR( 255 ) NOT NULL,
+  `email` DATETIME NOT NULL,
+--   Created_at | Sequlized Will Populate these.
+--   Updated_at | Sequalized Will Populate these.
+
+  PRIMARY KEY ( `id` ) 
+);
+
+CREATE TABLE `story_perms` (
+  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+  `story_id` VARCHAR( 255) NOT NULL,
+  `user_id` VARCHAR( 255 ) NOT NULL,
+--   Created_at | Sequlized Will Populate these.
+--   Updated_at | Sequalized Will Populate these.
+
+  PRIMARY KEY ( `id` ) 
+);
+
+CREATE TABLE `stories` (
+  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+  `first_name` VARCHAR( 255) NOT NULL,
+  `last_name` VARCHAR( 255 ) NOT NULL,
+  `email` DATETIME NOT NULL,
+--   Created_at | Sequlized Will Populate these.
+--   Updated_at | Sequalized Will Populate these.
+
+  PRIMARY KEY ( `id` ) 
+);
+
