c669fd574c93a5710b2be809bd15032a3d4b55ec
skendall <shannonckendall@gmail.com>
2019-03-09 11:10:31 -0500

    Pushing to API Branch Delete, Paragraph, Update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,58 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+module.exports = function (app) {
+  // CURRENT USER Posts a Paragraph Once Varified
+  app.get("/api/editor", function (req, res) {
+    var query = {};
+    if (req.query.user_id) {
+      query.UserId = req.query.user_id;
+    }
+    db.element
+      .findAll({
+        where: query,
+        include: [db.user_id]
+      })
+      .then(function (dbelement) {
+        res.json(dbelement);
+      });
+
+  });
+
+  // a Paragraph Once Varified
+  app.get("/api/editor", function (req, res) {
+    db.element
+      .findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [db.user_id]
+      })
+      .then(function (dbelement) {
+        res.json(dbelement);
+      });
+  });
+
+  // DELETE route for deleting posts inbody of editor 
+  app.delete("/api/editor", function (req, res) {
+    db.element.destroy({
+      where: {
+        id: req.body.id
+      }
+    }).then(function (dbelement) {
+      res.json(dbelement);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // PUT route for updating body of editor
+  app.put("/api/editor", function (req, res) {
+    db.element.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function (dbelement) {
+        res.json(dbelement);
+      });
   });
 };
