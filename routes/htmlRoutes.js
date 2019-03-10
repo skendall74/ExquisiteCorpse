d2981b906c6cbb1ad2eb4852fc9a366696dfffd2
Zach <95zmartin@gmail.com>
2019-03-10 12:19:34 -0400

    merge

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -36,42 +36,54 @@ const getOneStory = (tableName, id) => {
 
 module.exports = function (app) {
   // Load index page
+  // app.get("/:id?", function (req, res) {
+  //   let returnData = {};
+  //   let id;
+
+  //   if (req.params.id) {
+  //     id = req.params.id;
+  //   } else {
+  //     id = 1;
+  //   }
+
+  //   // Single promise that resolves once all of the promises passed as an iterable have resolved
+  //   Promise.all([
+  //     getOneStory("stories", id),
+  //     getAll("stories"),
+  //     getElementStory("element", id)
+  //   ]).then((data) => {
+  //     // Stringifies data
+  //     let jsonString = JSON.stringify(data);
+
+  //     // Converts data string into object and then places into an array
+  //     let dataArray = JSON.parse(jsonString);
+
+  //     // Stores returned data into object to be passed to front-end
+  //     returnData = {
+  //       oneStory: dataArray[0],
+  //       allStories: dataArray[1],
+  //       elements: dataArray[2],
+  //     }
+
+  //     console.log(returnData);
+  //     res.render("Index", returnData);
+  //   });
+  // });
+
+  // Load example page and pass in an example by id
   app.get("/:id?", function (req, res) {
-    let returnData = {};
-    let id;
+    res.render("index");
+  });
 
-    if (req.params.id) {
-      id = req.params.id;
-    } else {
-      id = 1;
-    }
-
-    // Single promise that resolves once all of the promises passed as an iterable have resolved
-    Promise.all([
-      getOneStory("stories", id),
-      getAll("stories"),
-      getElementStory("element", id)
-    ]).then((data) => {
-      // Stringifies data
-      let jsonString = JSON.stringify(data);
-
-      // Converts data string into object and then places into an array
-      let dataArray = JSON.parse(jsonString);
-
-      // Stores returned data into object to be passed to front-end
-      returnData = {
-        oneStory: dataArray[0],
-        allStories: dataArray[1],
-        elements: dataArray[2],
-      }
-
-      console.log(returnData);
-      res.render("Index", returnData);
+  app.get("/editor", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
+  app.get("/about", function (req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
